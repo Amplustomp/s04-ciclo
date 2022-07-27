e30d6bb5ccd5cd70985c39eb5811596d11b015ac
Sergio Cortés <sergio.cp@hotmail.es>
2022-07-27 21:56:52 +0000

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CicloComponente } from './ciclo/ciclo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CicloComponente
   ],
   imports: [
     BrowserModule
